4bb84789725db473f4df0867e0a1bcfdd1fd7209
burakbickioglu <burakbickioglu@gmail.com>
2021-11-22 20:16:09 +0300

    Add project files.

---
+++ ChatApp/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace ChatApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtLocalPort = new System.Windows.Forms.TextBox();
+            this.txtLocalIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtRemotePort = new System.Windows.Forms.TextBox();
+            this.txtRemoteIp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.listMessages = new System.Windows.Forms.ListBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbAlgoritm = new System.Windows.Forms.ComboBox();
+            this.btnEnterPassword = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLocalPort);
+            this.groupBox1.Controls.Add(this.txtLocalIp);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Me";
+            // 
+            // txtLocalPort
+            // 
+            this.txtLocalPort.Location = new System.Drawing.Point(120, 82);
+            this.txtLocalPort.Name = "txtLocalPort";
+            this.txtLocalPort.Size = new System.Drawing.Size(208, 38);
+            this.txtLocalPort.TabIndex = 1;
+            // 
+            // txtLocalIp
+            // 
+            this.txtLocalIp.Location = new System.Drawing.Point(120, 38);
+            this.txtLocalIp.Name = "txtLocalIp";
+            this.txtLocalIp.Size = new System.Drawing.Size(208, 38);
+            this.txtLocalIp.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PORT";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtRemotePort);
+            this.groupBox2.Controls.Add(this.txtRemoteIp);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(402, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(375, 133);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Friend";
+            // 
+            // txtRemotePort
+            // 
+            this.txtRemotePort.Location = new System.Drawing.Point(120, 82);
+            this.txtRemotePort.Name = "txtRemotePort";
+            this.txtRemotePort.Size = new System.Drawing.Size(208, 38);
+            this.txtRemotePort.TabIndex = 2;
+            //this.txtRemotePort.TextChanged += new System.EventHandler(this.txtRemotePort_TextChanged);
+            // 
+            // txtRemoteIp
+            // 
+            this.txtRemoteIp.Location = new System.Drawing.Point(120, 38);
+            this.txtRemoteIp.Name = "txtRemoteIp";
+            this.txtRemoteIp.Size = new System.Drawing.Size(208, 38);
+            this.txtRemoteIp.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PORT";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(797, 64);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(149, 54);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(145, 395);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(632, 38);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // listMessages
+            // 
+            this.listMessages.FormattingEnabled = true;
+            this.listMessages.ItemHeight = 31;
+            this.listMessages.Location = new System.Drawing.Point(12, 151);
+            this.listMessages.Name = "listMessages";
+            this.listMessages.Size = new System.Drawing.Size(934, 221);
+            this.listMessages.TabIndex = 43;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(797, 456);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(150, 40);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(145, 456);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(632, 38);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Message :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 459);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(121, 31);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Password :";
+            // 
+            // cmbAlgoritm
+            // 
+            this.cmbAlgoritm.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbAlgoritm.FormattingEnabled = true;
+            this.cmbAlgoritm.Items.AddRange(new object[] {
+            "SHA 256",
+            "SPN"});
+            this.cmbAlgoritm.Location = new System.Drawing.Point(797, 398);
+            this.cmbAlgoritm.Name = "cmbAlgoritm";
+            this.cmbAlgoritm.Size = new System.Drawing.Size(149, 39);
+            this.cmbAlgoritm.TabIndex = 5;
+            // 
+            // btnEnterPassword
+            // 
+            this.btnEnterPassword.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEnterPassword.Location = new System.Drawing.Point(797, 456);
+            this.btnEnterPassword.Name = "btnEnterPassword";
+            this.btnEnterPassword.Size = new System.Drawing.Size(150, 40);
+            this.btnEnterPassword.TabIndex = 10;
+            this.btnEnterPassword.Text = "Enter Password";
+            this.btnEnterPassword.UseVisualStyleBackColor = true;
+            this.btnEnterPassword.Click += new System.EventHandler(this.btnEnterPassword_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 506);
+            this.Controls.Add(this.btnEnterPassword);
+            this.Controls.Add(this.cmbAlgoritm);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.listMessages);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Chat App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtLocalPort;
+        private System.Windows.Forms.TextBox txtLocalIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtRemotePort;
+        private System.Windows.Forms.TextBox txtRemoteIp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.ListBox listMessages;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbAlgoritm;
+        private System.Windows.Forms.Button btnEnterPassword;
+    }
+}
+
---
+++ ChatApp/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+
+namespace ChatApp
+{
+    public partial class Form1 : Form
+    {
+        private Socket socket;
+        private EndPoint epLocal, epRemote;
+        private byte[] buffer;
+        private string senderPassword;
+        private string algorithmType;
+        private string message;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // set up socket
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress,true);
+
+            // get user ip
+            txtLocalIp.Text = GetLocalIp();
+            txtRemoteIp.Text = GetLocalIp();
+            txtLocalIp.Enabled = false;
+
+            btnEnterPassword.Hide();
+            
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                if (txtLocalPort.Text.Trim() != string.Empty && txtRemoteIp.Text.Trim() != string.Empty && txtRemotePort.Text.Trim() != string.Empty)
+                {
+
+                    txtRemotePort.Enabled = false;
+                    txtLocalPort.Enabled = false;
+                    txtRemoteIp.Enabled = false;
+                    // binding socket
+                    epLocal = new IPEndPoint(IPAddress.Parse(txtLocalIp.Text), Convert.ToInt32(txtLocalPort.Text));
+                    socket.Bind(epLocal);
+
+                    // connecting to remote ip
+                    epRemote = new IPEndPoint(IPAddress.Parse(txtRemoteIp.Text), Convert.ToInt32(txtRemotePort.Text));
+                    socket.Connect(epRemote);
+
+                    // listening the specific port
+                    buffer = new byte[1500];
+                    var a = socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote,
+                        new AsyncCallback(MessageCallBack), buffer);
+
+                    btnConnect.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Please enter all fields");
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+
+        }
+
+        private string GetLocalIp()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+                
+            }
+            return "127.0.0.1";
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Convert string message to byte[]
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                byte[] sendingMessage = new byte[1500];
+                
+                if (cmbAlgoritm.Text != "" && txtPassword.Text != "") 
+                {
+
+                    sendingMessage = aEncoding.GetBytes(cmbAlgoritm.Text.Trim() + "??__" + txtMessage.Text.Trim() + "??__" + sha256(txtPassword.Text.Trim()));
+
+                    //sendingPassword = aEncoding.GetBytes(sha256(txtPassword.Text));
+                    // Sending the encoded message
+                    socket.Send(sendingMessage);
+                    
+                    //socket.Send(sendingPassword);
+
+                    // adding to the listbox
+                    listMessages.Items.Add("Me: " + txtMessage.Text);
+                    txtMessage.Text = "";
+                    txtPassword.Text = "";
+
+                    ButtonCheck();
+
+                }
+                else
+                {
+                    MessageBox.Show("Please choose an encryption algorithm");
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void MessageCallBack(IAsyncResult aResult)
+        {
+            try
+            {
+                byte[] receiveData = new byte[1500];
+                receiveData = (byte[])aResult.AsyncState;
+                
+                // converting byte[] to string
+                ASCIIEncoding aEncoding = new ASCIIEncoding();
+                string receivedMessage = aEncoding.GetString(receiveData);
+                string[] post = receivedMessage.Split("??__");
+                // adding this message into Listbox
+                
+                listMessages.Items.Add("Friend: " + post[0] + "-"+ "Encrypted message");
+
+                algorithmType = post[0];
+                message = post[1];
+                senderPassword = post[2];
+
+
+                //MessageBox.Show(senderPassword);
+                buffer = new byte[1500];
+                socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote,
+                    new AsyncCallback(MessageCallBack), buffer);
+                ButtonCheck();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private string sha256(string text)
+        {
+            SHA256 sha256Encrypting = new SHA256CryptoServiceProvider();
+            byte[] bytes = sha256Encrypting.ComputeHash(Encoding.UTF8.GetBytes(text));
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var item in bytes)
+            {
+                builder.Append(item.ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+
+        private void btnEnterPassword_Click(object sender, EventArgs e)
+        {
+            
+            if (txtPassword.Text != "")
+            {
+                //MessageBox.Show("label: " + lblPassword.Text);
+                string password = sha256(txtPassword.Text);
+                //MessageBox.Show("password: " + password);
+
+                if (senderPassword.Contains(password))
+                {
+                    MessageBox.Show("Şifre doğru");
+                    listMessages.Items[^1] = "Friend: " + algorithmType + "-" + message;
+                    btnEnterPassword.Hide();
+                    btnSend.Show();
+                    txtMessage.Enabled = true;
+                    txtPassword.Text = "";
+                    //ButtonCheck();
+                }
+                else
+                {
+                    MessageBox.Show("Şifre yanlış");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Şifre giriniz");
+            }
+        }
+
+        
+        private void ButtonCheck()
+        {
+            string message = listMessages.Items[^1].ToString();
+            //MessageBox.Show(message);
+            string[] messagelist = message.Split(":");
+            if (messagelist[0] == "Friend")
+            {
+                btnEnterPassword.Show();
+                btnSend.Hide();
+                txtMessage.Enabled = false;
+            }
+            else
+            {
+                btnEnterPassword.Hide();
+                btnSend.Show();
+                txtMessage.Enabled = true;
+            }
+
+        }
+    }
+}
